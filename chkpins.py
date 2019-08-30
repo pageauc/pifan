289772ae45519251f57879165b5e3dab1984e066
Claude Pageau <pageauc@gmail.com>
2019-08-30 18:45:47 -0400

    Minor update

--- chkpins.py
+++ chkpins.py
@@ -6,11 +6,12 @@ script by Alex Eames https://raspi.tv/?p=6805
 from __future__ import print_function
 import RPi.GPIO as GPIO
 
-# Offer the user a choice of Pin or Port numbers and set numbering scheme accordingly
+# Offer the user a choice of Boare Pin
+# or BCM Port numbers and set numbering scheme accordingly
 print("GPIO Port Status Listing")
 print("------------------------")
-print("1 - Display by Port Number")
-print("2 - Display by BCM Port Numbering")
+print("1 - by Board Pin Number")
+print("2 - by BCM GPIO Pin Number")
 MENU_CHOICE = raw_input("Choice: ")
 print(' ')
 
@@ -18,12 +19,12 @@ if MENU_CHOICE == "1":
     GPIO.setmode(GPIO.BOARD)
     PORTS = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22,
              23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
-    PIN_TYPE = "Port Number"
+    PIN_TYPE = "Board Pin"
 else:
     GPIO.setmode(GPIO.BCM)
     PORTS = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26,
              14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
-    PIN_TYPE = "BCM Port"
+    PIN_TYPE = "BCM GPIO"
 
 # Using a dictionary as a lookup table to give a name to gpio_function() return code
 PORT_USE = {0:"GPIO.OUT", 1:"GPIO.IN", 40:"GPIO.SERIAL", 41:"GPIO.SPI",
